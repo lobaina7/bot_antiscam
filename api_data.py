39f571de6236c3ce67c5937f4ff12c30eeeff234
Alfredo Lobaina <93672385+lobaina7@users.noreply.github.com>
2023-05-22 17:11:21 -0500

    Update api_data.py

--- api_data.py
+++ api_data.py
@@ -1,8 +1,8 @@
-api_id='21342921'
-api_hash='a413f3fa5657085b7a4a239990fb0b56'
-chat_name='@teleantiscam_bot'
-admin_username='@lobaina7'
-bot_token = '5747268297:AAH-0s1NtC7X1sNooh1O-pIVPWPg2Wcgvuc'
+api_id=''
+api_hash=''
+chat_name=''
+admin_username=''
+bot_token = ''
 if __name__== '__main__':
       print(api_id)
       print(api_hash)
